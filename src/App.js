1c23989fde7f21d7b4825ed9ad6829f9d2f05013
#khansofficial <khanmahdi786@gmail.com>
2020-02-04 12:39:35 +0530

    continoous deploye test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ render(){
    <div className='main-container'>
 <h1 className='main-heading'>hello world</h1>
 <p className='main-para'>
-  This React App is made to Revise the 
+  This React App I made to Revise the 
   concepts i've Learned over the past few months.<br />
   Where ever you are today, i'm sure you're 
   HAPPY because YOU HAVE FIGURED IT OUT. 
