1b3b4311667a5dbc52d955b193e83b1017ae9006
#khansofficial <khanmahdi786@gmail.com>
2020-02-04 12:19:24 +0530

    continuos deployement check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ render(){
 <h1 className='main-heading'>hello world</h1>
 <p className='main-para'>
   This React App is made to Revise the 
-  concepts i've Learned over the past months.<br />
+  concepts i've Learned over the past few months.<br />
   Where ever you are today, i'm sure you're 
   HAPPY because YOU HAVE FIGURED IT OUT. 
 </p>
